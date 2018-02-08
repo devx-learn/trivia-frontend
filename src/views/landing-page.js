f0c7ba6171ad0c4bcb37f3c58a99e4c4668b9340
otaconn <cumpian.jj@gmail.com>
2018-02-07 16:11:31 -0800

    add views

---
+++ src/views/landing-page.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import SignUp from './components/signUp';
+import Chat from './components/chat';
+import SignIn from './components/signIn';
+import Trivia from './components/trivia';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="header">
+            <div>
+                <h1><u> Hello Trivia Team</u></h1>
+            </div>
+            <div className="container">
+                <div className="signin">
+                    <h3>Sign-In</h3>
+                    <SignIn />
+                </div>
+                <div className="signup">
+                    <h3> Sign-Up </h3>
+                    <SignUp />
+                </div>
+            </div>
+        </div>
+
+    );
+  }
+}
+
+export default App;
