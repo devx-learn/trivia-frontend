f35bacf7c8d91d8ba8cac0c982449f3798499210
Joshua <cumpian.jj@gmail.com>
2018-02-08 13:07:35 -0800

    renmaed views -> pages & clean App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import SignUp from './components/signUp';
-import Chat from './components/chat';
-import SignIn from './components/signIn';
-import Trivia from './components/trivia';
-import './App.css';
-import LandingPage from './views/landing-page'
-import GamePage from './views/game-page'
+import React, { Component } from 'react'
+import SignUp from './components/signUp'
+import Chat from './components/chat'
+import SignIn from './components/signIn'
+import Trivia from './components/trivia'
+import './App.css'
+import LandingPage from './pages/landing-page'
+import GamePage from './pages/game-page'
 
 class App extends Component {
-  render() {
-    return (
-        <div className="header">
-            <LandingPage />
-        </div>
-
-    );
-  }
+    render() {
+        return (
+            <div className="header">
+                <LandingPage />
+            </div>
+        )
+    }
 }
 
 export default App;
