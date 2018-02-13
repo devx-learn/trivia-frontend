26daae9abad0916ffacd2980d51eca8236d9d452
otaconn <cumpian.jj@gmail.com>
2018-02-13 14:17:59 -0800

    updated with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar } from 'react-bootstrap'
-// import SignUp from './components/signUp'
 import Chat from './components/chat'
 import SignIn from './components/signIn'
+import SignUp from './components/signUp'
 import Trivia from './components/trivia'
-import './App.css'
 import LandingPage from './pages/landing-page'
 import GamePage from './pages/game-page'
-import SignUp from './pages/sign-up-page'
+import LandingPageView from './pages/sign-up-page'
+import './App.css'
+
 
 class App extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+          user: [],
+          newUserSuccess: false,
+          errors: null
+      }
+  }
+
+  newUserSubmit(){}
+
+  // componentWillMount(){
+  //   fetch(`${this.state.apiUrl}/user`)
+  //   .then((rawResponse) =>{
+  //       return rawResponse.json()
+  //   })
+  //   .then((parsedResponse)=>{
+  //       this.setState({user: parsedResponse.user})
+  //       console.log(this.state.user);
+  //   })
+  // }
+
+
+  handleNewUser(params){
+    fetch(`${this.state.apiUrl}/user`,
+        {
+            body: JSON.stringify(params),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: "POST"
+        }
+    )
+    .then((rawResponse)=>{
+        return rawResponse.json()
+    })
+    .then((parsedResponse) =>{
+        if(parsedResponse.errors){
+            this.setState({errors: parsedResponse.errors})
+        }else{
+            const user = Object.assign([], this.state.user)
+            user.push(parsedResponse.user)
+            this.setState({
+                user: user,
+                errors: null,
+                newUserSuccess: true
+        })
+       }
+    })
+  }
+
+
     render() {
         return (
             <Router>
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,78 +1,157 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import {
-  Button,
   Col,
   ControlLabel,
   FormGroup,
   FormControl,
+  Button,
   Row,
-} from 'react-bootstrap'
+  HelpBlock,
+  Alert
+} from 'react-bootstrap';
 
 class SignUp extends Component {
     constructor(props){
         super(props)
         this.state = {
             form: {
-                username: '',
-                password: ''
+                firstName: '',
+                lastName: '',
+                email: '',
+                encryptedPassword: '',
+                authToken: '',
+                authTokenExpiration: '',
+                salt: ''
             }
         }
     }
 
-    handleChange(e){
+    handleChange(event){
         const formState = Object.assign({}, this.state.form)
-        formState[e.target.name] = e.target.value
+        formState[event.target.name] = event.target.value
         this.setState({form: formState})
     }
 
-  render() {
-    return (
-      <form>
-        <Row>
-          <Col xs={6}>
-            <FormGroup>
-              <ControlLabel id="username">Username</ControlLabel>
-              <FormControl
-                type="text"
-                name="username"
-                value={this.state.form.username}
+    handleSubmit(){
+        this.props.onSubmit(this.state.form)
+        console.log(this.state.form)
+    }
 
-                onChange={this.handleChange.bind(this)}
+    errorsFor(attribute){
+        var errorString = ""
+        if(this.props.errors){
+            const errors = this.props.errors.filter(error => error.param === attribute )
+            if(errors){
+                errorString = errors.map(error => error.msg ).join(", ")
+            }
+        }
+        return errorString === "" ? null : errorString
+    }
 
+    render(){
+        return (
+            <form>
+                <Row>
+                    <Col xs={6}>
+                        {this.props.errors &&
+                            <Alert bsStyle="danger">
+                                Please check the form and try again.
+                            </Alert>
+                        }
+                    </Col>
+                </Row>
 
-              />
-            </FormGroup>
-          </Col>
-        </Row>
+            <div class='forms'>
+                 <Row>
+                   <Col xs={6}>
+                   <FormGroup
+                     id="firstName-form-group"
+                     validationState={this.errorsFor('firstName') && 'error'}>
+                     <ControlLabel id="firstName">First Name</ControlLabel>
+                     <FormControl
+                       type="text"
+                       name="firstName"
+                       value={this.state.form.firstName}
+                       onChange={this.handleChange.bind(this)}
+                     />
+                    {this.errorsFor('firstName') &&
+                        <HelpBlock id="firstName-help-block">{this.errorsFor('firstName')}</HelpBlock>
+                    }
+                   </FormGroup>
+                   </Col>
+                 </Row>
 
-        <Row>
-          <Col xs={6}>
-            <FormGroup>
-              <ControlLabel id="password">Password</ControlLabel>
-              <FormControl
-                type="password"
-                name="password"
-                value={this.state.form.password}
-                onChange={this.handleChange.bind(this)}
+                 <Row>
+                   <Col xs={6}>
+                    <FormGroup
+                        id="lastName-form-group"
+                        validationState={this.errorsFor('lastName') && 'error'}>
+                       <ControlLabel id="lastName">Last Name</ControlLabel>
+                       <FormControl
+                       type="text"
+                       name="lastName"
+                       onChange={this.handleChange.bind(this)}
+                       value={this.state.form.lastName}
+                       />
+                       {this.errorsFor('lastName') &&
+                       <HelpBlock id="lastName-help-block">{this.errorsFor('lastName')}</HelpBlock>
+                   }
+                     </FormGroup>
+                   </Col>
+                 </Row>
 
-              />
-            </FormGroup>
-          </Col>
-        </Row>
+                 <Row>
+                   <Col xs={6}>
+                     <FormGroup
+                        id="email-form-group"
+                        validationState={this.errorsFor('email') && 'error'}>
+                       <ControlLabel id="email">Email</ControlLabel>
+                       <FormControl
+                       type="text"
+                       name="email"
+                       onChange={this.handleChange.bind(this)}
+                       value={this.state.form.email}
+                       />
+                       {this.errorsFor('email') &&
+                       <HelpBlock id="email-help-block">{this.errorsFor('email')}</HelpBlock>
+                        }
+                     </FormGroup>
+                   </Col>
+                 </Row>
 
-        <Row>
-          <Col xs={6}>
-          <Link to="/games">
-            <Button id="submit">Create a Trivia Account!</Button>
-            </Link>
-          </Col>
-        </Row>
+                 <Row>
+                   <Col xs={6}>
+                     <FormGroup
+                        id="encryptedPassword-form-group"
+                        validationState={this.errorsFor('encryptedPassword') && 'error'}>
+                       <ControlLabel id="encryptedPassword">Password</ControlLabel>
+                       <FormControl
+                       type="password"
+                       name="encryptedPassword"
+                       onChange={this.handleChange.bind(this)}
+                       value={this.state.form.encryptedPassword}
+                       />
+                       {this.errorsFor('encryptedPassword') &&
+                       <HelpBlock id="encryptedPassword-help-block">{this.errorsFor('encryptedPassword')}</HelpBlock>
+                        }
+                     </FormGroup>
+                   </Col>
+                 </Row>
 
-      </form>
+                </div>
 
-    )
-  }
+                 <Row>
+                   <Col xs={6}>
+                     <Button
+                         id="submit"
+                         onClick={this.handleSubmit.bind(this)}
+                     >Create a Trivia Account!</Button>
+                   </Col>
+                 </Row>
+
+               </form>
+        );
+    }
 }
 
 export default SignUp
